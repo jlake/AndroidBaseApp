81bb20296ac7bc81d2bc82ef510a3102bc74c475
Jlake Ou <ouzhiwei@gmail.com>
2016-06-03 16:11:03 +0900

    First commit

---
+++ app/src/main/java/bz/adore/androidbaseapp/AnotherActivity.java
@@ -0,0 +1,36 @@
+package bz.adore.androidbaseapp;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AnotherActivity extends AppCompatActivity {
+    protected ActionBar mActionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        mActionBar = getSupportActionBar();
+        mActionBar.setDisplayShowTitleEnabled(true);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onBackPressed() {
+        finish();
+    }
+}
